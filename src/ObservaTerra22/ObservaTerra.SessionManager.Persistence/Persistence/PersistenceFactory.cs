61f31cb4fae3e2db6b597ea0fe6c8e2b6f0c48d1
Raul Estrada <raulestradaballesteros@gmail.com>
2014-04-17 19:12:48 +0200

    Added the SessionManager Persistence package with methods to work with Users: FindByUsername, SaveUser and DeleteUser. Still missing users database

---
+++ src/ObservaTerra22/ObservaTerra.SessionManager.Persistence/Persistence/PersistenceFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObservaTerra.SessionManager.Persistence
+{
+    class PersistenceFactory
+    {
+        /// <summary>
+        /// Factory method that creates IUserPersistenceManagement instances
+        /// </summary>
+        /// <returns>A IUserPersistenceManagement instance</returns>
+        public static IUserPersistenceManagement GetUserPersistenceManagement()
+        {
+            return UserPersistenceManagement.GetInstance();
+        }
+    }
+}
