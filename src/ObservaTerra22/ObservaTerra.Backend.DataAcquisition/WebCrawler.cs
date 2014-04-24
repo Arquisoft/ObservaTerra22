2edbfdab8a70b8c26479744e88d8f3eb11865cbf
Maria Leyva <marialv197@gmail.com>
2014-04-24 18:31:54 +0200

    WebCrawler implementation

---
+++ src/ObservaTerra22/ObservaTerra.Backend.DataAcquisition/WebCrawler.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abot.Crawler;
+using Abot.Poco;
+
+namespace ObservaTerra.BackEnd.DataAcquisition
+{
+    public class WebCrawler
+    {
+        public PoliteWebCrawler Crawler { get; set; }
+        public WebCrawler()
+        {
+            init();
+        }
+  
+
+        public CrawlResult crawl(Uri uri)
+        {
+            return Crawler.Crawl(uri);
+        }
+        public void init() {
+
+            CrawlConfiguration crawlConfig = new CrawlConfiguration();
+            crawlConfig.CrawlTimeoutSeconds = 200;
+            crawlConfig.MaxConcurrentThreads = 100;
+            crawlConfig.DownloadableContentTypes = "text/html";
+            crawlConfig.MaxPagesToCrawl = 10000;
+            crawlConfig.IsExternalPageLinksCrawlingEnabled = false;
+            crawlConfig.IsExternalPageLinksCrawlingEnabled = false;
+            crawlConfig.UserAgentString = "abot v1.0 http://code.google.com/p/abot";
+            crawlConfig.MaxCrawlDepth = 100;
+
+            Crawler = new PoliteWebCrawler(crawlConfig, null, null, null, null, null, null, null, null);
+
+            //crawler.PageCrawlStartingAsync += crawler_ProcessPageCrawlStarting;
+            Crawler.PageCrawlCompletedAsync += crawler_ProcessPageCrawlCompleted;
+            
+        }
+        static void crawler_ProcessPageCrawlCompleted(object sender, PageCrawlCompletedArgs e)
+        {
+            CrawledPage crawledPage = e.CrawledPage;
+            //System.IO.File.WriteAllText(System.IO.Directory.GetCurrentDirectory() + "/crawlTmp.txt", crawledPage.Content.Text);
+
+            if (crawledPage.Uri.AbsoluteUri.Contains(".csv"))
+            {
+                using (System.Net.WebClient client = new System.Net.WebClient())
+                {
+                    client.DownloadFile(crawledPage.Uri, System.IO.Directory.GetCurrentDirectory() + "/prueba" + new Random().Next() + ".csv");
+                    Console.WriteLine("Downloaded file from {0}", crawledPage.Uri.AbsoluteUri);
+                }
+            }
+
+            if (crawledPage.WebException != null || crawledPage.HttpWebResponse.StatusCode != System.Net.HttpStatusCode.OK)
+                Console.WriteLine("Crawl of page failed {0}", crawledPage.Uri.AbsoluteUri);
+            else
+                Console.WriteLine("Crawl of page succeeded {0}", crawledPage.Uri.AbsoluteUri);
+
+            if (string.IsNullOrEmpty(crawledPage.Content.Text))
+                Console.WriteLine("Page had no content {0}", crawledPage.Uri.AbsoluteUri);
+        }
+    }
+}
