b8c091d1b620d9bd152a0b9789ff3872b0e8c691
zapiko <d.zapico@hotmail.com>
2014-04-30 18:16:50 +0200

    Change Controllers for WebAPI

--- ObservaTerra22/ObservaTerra.DomainModel/DomainModel.Designer.cs
+++ ObservaTerra22/ObservaTerra.DomainModel/DomainModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Raúl Estrada\Desktop\asw\ObservaTerra22\src\ObservaTerra22\ObservaTerra.DomainModel\DomainModel.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Repos\ObservaTerra22\ObservaTerra22\ObservaTerra.DomainModel\DomainModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
---
+++ ObservaTerra22/ObservaTerra.Web/Controllers/ObservationController.cs
@@ -0,0 +1,36 @@
+﻿using ObservaTerra.Backend.DataQuery;
+using ObservaTerra.Backend.HumanData;
+using ObservaTerra.DomainModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ObservaTerra.Backend.WebService.Controllers
+{
+    public class ObservationController : ApiControllerLogin
+    {
+        public Observation Get(string token, int id)
+        {
+            User user = GetUserByToken(token);
+            IObservationQuery query = DataQueryFactory.GetObservationQueries(user);
+            return query.GetObservation(id);
+        }
+
+        public IEnumerable<Observation> Get(string token, string partialname)
+        {
+            User user = GetUserByToken(token);
+            IObservationQuery query = DataQueryFactory.GetObservationQueries(user);
+            return query.FindObservations(partialname);
+        }
+
+        public void Post(string token, Observation observation)
+        {
+            User user = GetUserByToken(token);
+            IObservationHumanData humandata = HumanDataFactory.GetObservationHumanData(user);
+            humandata.AddObservation(observation);
+        }
+    }
+}
