49c71575b107daaefed879aeb8c5eabbaecfbd59
Maria Leyva <marialv197@gmail.com>
2014-05-03 19:43:54 +0200

    Now reading datasources from xml file

---
+++ ObservaTerra22/ObservaTerra.Backend.DataAcquisition/CrawlerService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Xml.Serialization;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ObservaTerra.BackEnd.DataAcquisition;
+
+namespace ObservaTerra.Backend.DataAcquisition
+{
+    class CrawlerService
+    {
+        static void Main(string[] args)
+        {
+            CrawlerService CR = new CrawlerService();
+            CR.AddUri("http://apps.who.int/gho/athena/data/GHO/WHOSIS_000002,WHOSIS_000001,WHOSIS_000015?profile=xtab&format=html&x-topaxis=GHO;SEX&x-sideaxis=COUNTRY;YEAR&x-title=table&filter=COUNTRY:*;REGION:AFR;REGION:AMR;REGION:SEAR;REGION:EUR;REGION:EMR;REGION:WPR;SEX:*");
+            CR.Crawl();
+        }
+        public WebCrawler Crawler { get; set; }
+        public DataSources ToCrawl { get; set; }
+        public CrawlerService()
+        {
+            Crawler = new WebCrawler();
+            ToCrawl = ReadFromFile();
+        }
+        public void AddUri(string uri)
+        {
+
+            ToCrawl.Add(uri);
+            UpdateFile();
+        }
+
+        private void UpdateFile()
+        {
+            XmlSerializer writer = new XmlSerializer(typeof(DataSources));
+            using (FileStream file = File.OpenWrite("sources.xml"))
+            {
+                writer.Serialize(file, ToCrawl);
+            }
+        }
+        public void RemoveUri(string uri)
+        {
+            ToCrawl.Remove(uri);
+            UpdateFile();
+        }
+        public void Crawl()
+        {
+            // ToCrawl= readFromFile();
+            ToCrawl.Sources.ForEach(x => Crawler.crawl(new Uri(x)));
+        }
+
+        private DataSources ReadFromFile()
+        {
+
+            XmlSerializer reader = new XmlSerializer(typeof(DataSources));
+            using (FileStream input = File.OpenRead(Directory.GetCurrentDirectory() + "/sources/sources.xml"))
+            {
+                return reader.Deserialize(input) as DataSources;
+            }
+        }
+    }
+}
---
+++ ObservaTerra22/ObservaTerra.Backend.DataAcquisition/DataSources.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObservaTerra.Backend.DataAcquisition
+{
+    [Serializable]
+    public class DataSources
+    {
+        public List<string> Sources { get; set; }
+        public DataSources()
+        {
+            Sources = new List<string>();
+        }
+
+        public void Add(string u)
+        {
+            if (!Sources.Contains(u))
+                Sources.Add(u);
+        }
+        public void Remove(string u)
+        {
+            Sources.Remove(u);
+        }
+    }
+}
